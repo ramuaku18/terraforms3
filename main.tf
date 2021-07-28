2d0bd606a92f5a564b7a4186d88194457dde1010
ramuaku18 <88086138+ramuaku18@users.noreply.github.com>
2021-07-28 16:43:03 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region = "${var.region}"
+}
+
+module "s3" {
+    source = "terraform-aws-modules/s3-bucket/aws"
+    #bucket name should be unique
+    bucket_name = "rambucket"       
+}
---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "aws_access_key" {
+default = "AKIATSHK2TQFCHXZU4OQ"
+}
+variable "aws_secret_key" {
+default = "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"
+ }
+variable "region" {
+    default = "region"
+}
